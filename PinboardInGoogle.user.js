a7b06fd120c5f338fd95d5a332cecace7b9d4cd7
Nicolas Hoizey <nicolas@hoizey.com>
2015-03-06 00:43:33 +0100

    Automaticaly set user's secret from Pinboard pin page
    Fixes #4

--- PinboardInGoogle.user.js
+++ PinboardInGoogle.user.js
@@ -23,57 +23,98 @@
 // @license       MIT; https://github.com/nhoizey/PinboardInGoogle/blob/master/LICENSE
 // @icon          https://pinboard.in/bluepin.gif
 // @namespace     com.gasteroprod.lab
-// @version       1.4
+// @version       1.5
 // @downloadURL   https://github.com/nhoizey/PinboardInGoogle/raw/master/PinboardInGoogle.user.js
 // @include       http://www.google.*/*
 // @include       https://www.google.*/*
+// @include       https://feeds.pinboard.in/rss/*
 // @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @grant         GM_deleteValue
 // @grant         GM_xmlhttpRequest
 // ==/UserScript==
 
-(function(w, d) {
+(function() {
   var msg = '',
       html = '',
       queryStr = '',
       jsonUrl = '',
       jsonData = '';
 
+  function handleSecret() {
+    var secret = window.location.href.replace(/^.*\/secret:([^\/]+)\/.*$/g, "$1"),
+        user = window.location.href.replace(/^.*\/u:([^\/]+)\/.*$/g, "$1");
+    GM_setValue('secret', secret);
+    GM_setValue('user', user);
+  }
+
+  function initResultBox() {
+    // Create the results container
+    $('#res').prepend('<div id="PinboardInGoogle" class="small"></div>');
+
+    // Add some style to it
+    $('head').append('\
+      <style>\
+      #PinboardInGoogle { background-color: #eee; padding: .2em; margin-bottom: 1em; }\
+      #PinboardInGoogle p { margin: .5em 0; }\
+      #PinboardInGoogle p.title { color: #aaa; padding-left: 20px; background: url(https://pinboard.in/bluepin.gif) left center no-repeat; }\
+      #PinboardInGoogle .how, #PinboardInGoogle .reset { float: right; font-size: .8em; }\
+      #PinboardInGoogle.small li:nth-child(n+4) { display: none; }\
+      </style>'
+    );
+  }
+
   function handleSearch() {
-    console.info('PinboardInGoogle userscript running…');
+    var user = GM_getValue('user'),
+        secret = GM_getValue('secret');
 
-    // Get the query value
-    queryStr = d.querySelector('form[role=search] input[name=q]').value;
+    // Remove any previously created box
+    $('#PinboardInGoogle').remove();
 
-    if (undefined !== queryStr) {
-      // Translate the query string to a list of tags
-      queryStr = queryStr.replace(/[^\w]/g, " ");
-      queryStr = queryStr.replace(/ +/g, " ");
-      queryStr = queryStr.replace(/^ | $/g, "");
+    if (undefined === user || undefined === secret) {
+      initResultBox();
+      html = '<p class="title">Pinboard bookmarks</p>';
+      html += '<p>To configure this:</p><ol>';
+      html += '<li style="list-style: decimal inside">go to <a href="https://pinboard.in/">your Pinboard homepage</a></li>';
+      html += '<li style="list-style: decimal inside">click on the orange "<span style="color: orange">RSS</span>" link on top right</li>';
+      html += '<li style="list-style: decimal inside">come back here</li></ol>';
+      $('#PinboardInGoogle').html(html);
+    } else {
+      // Get the query value
+      queryStr = window.document.querySelector('form[role=search] input[name=q]').value;
 
-      // Generate Pinboard API JSON URL
-      // https://feeds.pinboard.in/json/v1/u:nhoizey/t:24-70mm/t:canon?count=20
-      queryStr = queryStr.replace(/\s*(\w+)\s*/g, "/t:$1");
-      jsonUrl = 'https://feeds.pinboard.in/json/v1/u:nhoizey' + queryStr;
-      jsonData = 'count=20'; // todo: doesn't seem to work
+      if (undefined !== queryStr) {
+        // Translate the query string to a list of tags
+        queryStr = queryStr.replace(/[^\w]/g, " ");
+        queryStr = queryStr.replace(/ +/g, " ");
+        queryStr = queryStr.replace(/^ | $/g, "");
 
-      // Request the API
-      GM_xmlhttpRequest({
-        method: 'GET',
-        url: jsonUrl,
-        data: jsonData,
-        timeout: 10000, // 10 seconds
-        headers: {
-          'User-Agent': 'PinboardInGoogle',
-          'Accept': 'application/json'
-        },
-        onload: function(responseDetails) {
-          parseFeed(responseDetails);
-        },
-        onerror: function(responseDetails) {
-          // Show the error message
-          console.log('error…');
-        }
-      });
+        // Generate Pinboard API JSON URL
+        // https://feeds.pinboard.in/json/v1/secret:....../u:nhoizey/t:24-70mm/t:canon?count=20
+        queryStr = queryStr.replace(/\s*(\w+)\s*/g, "/t:$1");
+        jsonUrl = 'https://feeds.pinboard.in/json/v1/secret:' + secret + '/u:' + user + queryStr;
+        jsonData = 'count=20'; // todo: doesn't seem to work
+
+        // Request the API
+        GM_xmlhttpRequest({
+          method: 'GET',
+          url: jsonUrl,
+          data: jsonData,
+          timeout: 10000, // 10 seconds
+          headers: {
+            'User-Agent': 'PinboardInGoogle',
+            'Accept': 'application/json'
+          },
+          onload: function(responseDetails) {
+            parseFeed(responseDetails);
+          },
+          onerror: function(responseDetails) {
+            // Show the error message
+            console.log('error…');
+          }
+        });
+      }
     }
   }
 
@@ -83,19 +124,7 @@
         max = Math.min(nb, 20);
 
     if (nb > 0) {
-      // Create the results container
-      $('#res').prepend('<div id="PinboardInGoogle" class="small"></div>');
-
-      // Add some style to it
-      $('head').append('\
-        <style>\
-        #PinboardInGoogle { background-color: #eee; padding: .2em; margin-bottom: 1em; }\
-        #PinboardInGoogle p { margin: .5em 0; }\
-        #PinboardInGoogle p.title { color: #aaa; padding-left: 20px; background: url(https://pinboard.in/bluepin.gif) left center no-repeat; }\
-        #PinboardInGoogle .how { float: right; font-size: .8em; }\
-        #PinboardInGoogle.small li:nth-child(n+4) { display: none; }\
-        </style>'
-      );
+      initResultBox();
 
       html = '<p class="title">Pinboard bookmarks <a class="how" href="https://github.com/nhoizey/PinboardInGoogle">how does it work?</a></p>';
       html += '<ol>';
@@ -106,17 +135,21 @@
         html += '<span class="st">' + bookmarks[i].n + '</span>';
         html += '</li>';
       }
+      html += '<li class="g"><div class="rc"><h3 class="r">&gt;&gt; <a href="https://pinboard.in' + queryStr + '">these bookmarks on Pinboard</a></h3></div></li>';
       html += '</ol><p>';
       if (nb > 3) {
-        html += '<a class="more" href="" onclick="document.querySelector(\'#PinboardInGoogle\').className=\'\'; t = document.querySelector(\'#PinboardInGoogle .more\'); t.parentNode.removeChild(t); return false;">+ show more</a>';
+        html += '<a class="more" href="#" onclick="document.querySelector(\'#PinboardInGoogle\').className=\'\'; t = document.querySelector(\'#PinboardInGoogle .more\'); t.parentNode.removeChild(t); return false;">+ show more bookmarks</a>';
       }
       html += '&nbsp;';
-      html += '<a href="https://pinboard.in/u:nhoizey' + queryStr + '" style="float: right;">these bookmarks on Pinboard</a>';
       html += '</p>';
     }
     $('#PinboardInGoogle').html(html);
   }
 
-  $(document.querySelector('form[role=search] input[name=q]')).on('change', handleSearch);
-  handleSearch();
-})(window, document);
+  if (window.location.href.match(/^https:\/\/feeds\.pinboard\.in\/rss\/.*$/)) {
+    handleSecret();
+  } else {
+    $(window.document.querySelector('form[role=search] input[name=q]')).on('change', handleSearch);
+    handleSearch();
+  }
+})();
